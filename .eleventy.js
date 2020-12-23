f7ad9d674f42b3c2d77ffb22476c837f1a14c518
Andrew Harvard <andrew.t.harvard@gmail.com>
2020-12-23 14:07:45 -0600

    fix JS issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,14 @@
 const CleanCSS = require('clean-css');
-const Terser = require('terser');
+const {minify} = require('terser');
 const pluginDate = require('eleventy-plugin-date');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
 
   eleventyConfig.addPlugin(pluginDate, {
-    // Specify custom date formats
     formats: {
-      // Change the readableDate filter to use abbreviated months.
       readableDate: {year: 'numeric', month: 'short', day: 'numeric'},
-      // Add a new filter to format a Date to just the month and year.
       readableMonth: {year: 'numeric', month: 'long'},
-      // Add a new filter using formatting tokens.
       timeZone: 'z',
     },
   });
@@ -21,14 +17,19 @@ module.exports = function (eleventyConfig) {
     return new CleanCSS({}).minify(code).styles;
   });
 
-  eleventyConfig.addFilter('jsmin', function (code) {
-    let minified = Terser.minify(code);
-    if (minified.error) {
-      console.log('Terser error: ', minified.error);
-      return code;
-    }
-    return minified.code;
-  });
+  eleventyConfig.addNunjucksAsyncFilter(
+    'jsmin',
+    async function (code, callback) {
+      try {
+        const minified = await minify(code);
+        callback(null, minified.code);
+      } catch (err) {
+        console.error('Terser error: ', err);
+        // Fail gracefully.
+        callback(null, code);
+      }
+    },
+  );
 
   eleventyConfig.addPairedShortcode('pattern', function (content, patternName) {
     return `
